e202ea5ee9f091f8ba46d854bfdf25f358ed75d5
zjx <jiaxiang.zhu@qq.com>
2023-06-14 21:00:54 +0800

    cookies状态保持

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -1,6 +1,7 @@
 import json
 import re
 
+from MySQLdb import DatabaseError
 from django.shortcuts import render
 
 # Create your views here.
@@ -105,6 +106,26 @@ class RegisterView(View):
         # 以上两种方式都可以数据入库，但是存在一个问题，密码没有加密
 
         # 密码加密
-        user = User.objects.create_user(username=username, password=password, mobile=mobile)
+        try:
+            user = User.objects.create_user(username=username, password=password, mobile=mobile)
+        except DatabaseError:
+            return JsonResponse({'code': 400, 'errmsg': '注册失败!'})
+
+        # 如何设置session信息
+        # request.session['user_id']=user.id
+        # 系统（Django）为我们提供了状态保持的方法
+        from django.contrib.auth import login
+        # request, user,
+        # 状态保持 -- 登录用户的状态保持
+        login(request, user)
         # 5. 返回响应
         return JsonResponse({'code': 0, 'errmsg': 'ok'})
+
+"""
+如果需求是注册成功后即表示用户认证通过，那么此时可以在注册成功后实现状态保持（注册成功即已经登录）
+如果需求是注册成功后不表示用户认证通过，那么此时不用在注册成功后实现状态保持（注册成功，单独登录）
+
+实现状态保持主要有两种方式：
+    在客户端存储信息使用Cookie
+    在服务器端存储信息使用Session
+"""
